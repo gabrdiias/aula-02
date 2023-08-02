8ff673f58c74a5c39403c6a5dc00977cf1348206
gabrdiias <127976037+gabrdiias@users.noreply.github.com>
2023-08-02 11:31:24 -0300

    Add files via upload

---
+++ ativ02.js
@@ -0,0 +1,44 @@
+// Questão 01
+
+let kelvin = 293
+
+// Questão 02
+//Essa linha de código indica o valor que foi atribuído a váriavel "kelvin"
+
+// Questão 03
+
+kelvin = 293;
+console.log(kelvin - '273')
+let celsius = kelvin - 273
+console.log(celsius + '°' + 'Essa é a temperatura em g°C')
+
+// Questão 04
+// Novamente eu chamei pela variável Kelvin, para realizar o cálculo correto baseado no valor dessa variável, assim, fiz a subtração e obtive o valor em celsius
+
+// Questão 05
+
+console.log (celsius * 9/5 + 32 + ' ' + 'Essa é a temperatura em fahrenheit')
+
+// Questão 06
+//Nesta linha de código, eu busco ao valor da variável 'celsius', para a partir dele, realizar as operações que indicarão a temperatura em fahrenheit, uso o '+', para a concatenação e também uso o símbolo como operador matemático no início da linha de código
+
+//Questão 07
+
+let fahrenheit = celsius * 9/5 + 32
+fahrenheit = Math.floor(fahrenheit)
+console.log (fahrenheit + ' ' + 'Esta é a temperatura em fahrenheit')
+
+//Questão 08
+//Nesta atividade eu criei mais uma variável para 'fahrenheit' e dei como valor para ela, o resultado da operação presente acima. Após isso, rodei isso e adicionei um texo para informar o usuário da temperatura.
+
+//Questão 11
+
+kelvin = 0
+console.log(kelvin - '273' * 9/5 + 32)
+
+//Questão 12
+
+let newton = celsius * (33/100);
+newton = Math.floor(newton)
+console.log(`A temperatura é ${newton} graus Newton.`);
+
